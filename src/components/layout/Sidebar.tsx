8d1552f31628352f994bf5b9f79ceb62a66e7699
NaimurAlltime <naimur2231@gmail.com>
2024-01-21 01:12:48 +0600

    Render Sidebar Based On Role

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,6 +1,8 @@
 import { Layout, Menu } from "antd";
 import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
 import { adminPaths } from "../../routes/admin.routes";
+import { facultyPaths } from "../../routes/faculty.routes";
+import { studentPaths } from "../../routes/student.routes";
 
 const { Sider } = Layout;
 
@@ -11,6 +13,23 @@ const userRole = {
 };
 
 function Sidebar() {
+  const role = "admin";
+  let sidebarItems;
+
+  switch (role) {
+    case userRole.ADMIN:
+      sidebarItems = sidebarItemsGenerator(adminPaths, userRole.ADMIN);
+      break;
+    case userRole.FACULTY:
+      sidebarItems = sidebarItemsGenerator(facultyPaths, userRole.FACULTY);
+      break;
+    case userRole.STUDENT:
+      sidebarItems = sidebarItemsGenerator(studentPaths, userRole.STUDENT);
+      break;
+
+    default:
+      break;
+  }
   return (
     <Sider breakpoint="lg" collapsedWidth="0">
       <div
@@ -28,7 +47,7 @@ function Sidebar() {
         theme="dark"
         mode="inline"
         defaultSelectedKeys={["4"]}
-        items={sidebarItemsGenerator(adminPaths, "admin")}
+        items={sidebarItems}
       />
     </Sider>
   );
