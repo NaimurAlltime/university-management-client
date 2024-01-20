6d90b22f62ad4f404926a0fce2515eda587cec3b
NaimurAlltime <naimur2231@gmail.com>
2024-01-21 00:49:34 +0600

    refactor sidebar menu item with utils function

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Layout, Menu } from "antd";
 import { Outlet } from "react-router-dom";
-import { adminSidebarItems } from "../../routes/admin.routes";
+import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+import { adminPaths } from "../../routes/admin.routes";
 
 const { Header, Content, Footer, Sider } = Layout;
 
@@ -61,7 +62,7 @@ function MainLayout() {
           theme="dark"
           mode="inline"
           defaultSelectedKeys={["4"]}
-          items={adminSidebarItems}
+          items={sidebarItemsGenerator(adminPaths, "admin")}
         />
       </Sider>
       <Layout>
--- src/utils/routesGenerator.ts
+++ src/utils/routesGenerator.ts
@@ -1,16 +1,4 @@
-import { ReactNode } from "react";
-
-export type TRoute = {
-  path: string;
-  element: ReactNode;
-};
-
-export type TUserPath = {
-  name: string;
-  path?: string;
-  element?: ReactNode;
-  children?: TUserPath[];
-};
+import { TRoute, TUserPath } from "../types";
 
 export const routeGenerator = (items: TUserPath[]) => {
   const routes = items.reduce((acc: TRoute[], item) => {
