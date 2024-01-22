ff4ed73c8a226d0a0d1452af5f4df4ffd97e58af
NaimurAlltime <naimur2231@gmail.com>
2024-01-22 22:45:18 +0600

    BaseQuery To Send AccessToken

--- src/pages/admin/CourseManagement/GetAllCourse.tsx
+++ src/pages/admin/CourseManagement/GetAllCourse.tsx
@@ -1,4 +1,8 @@
+import { useGetAllCourseQuery } from "../../../redux/features/course/courseApi";
+
 function GetAllCourse() {
+  const { courses } = useGetAllCourseQuery(undefined);
+  console.log(courses);
   return <div>GetAllCourse</div>;
 }
 
