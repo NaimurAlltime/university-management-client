15855fbb7ec7293e37eddf9faa733dbb35e5cb48
Naimur Rahman <naimur2231@gmail.com>
2025-02-24 04:10:35 +0600

    project updated

--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -3,17 +3,14 @@ import CreateAdmin from '../pages/admin/userManagement/CreateAdmin';
 import CreateFaculty from '../pages/admin/userManagement/CreateFaculty';
 import CreateStudent from '../pages/admin/userManagement/CreateStudent';
 import AcademicDepartment from '../pages/admin/academicManagement/AcademicDepartment';
-import AcademicFaculty from '../pages/admin/academicManagement/AcademicFaculty';
 import AcademicSemester from '../pages/admin/academicManagement/AcademicSemester';
 import CreateAcademicDepartment from '../pages/admin/academicManagement/CreateAcademicDepartment';
-import CreateAcademicFaculty from '../pages/admin/academicManagement/CreateAcademicFaculty';
 import CreateAcademicSemester from '../pages/admin/academicManagement/CreateAcademicSemester';
 import StudentData from '../pages/admin/userManagement/StudentData';
 import StudentDetails from '../pages/admin/userManagement/StudentDetails';
 import SemesterRegistration from '../pages/admin/courseManagement/SemesterRegistration';
 import RegisteredSemesters from '../pages/admin/courseManagement/RegisteredSemesters';
 import CreateCourse from '../pages/admin/courseManagement/CreateCourse';
-import Courses from '../pages/admin/courseManagement/Courses';
 import OfferCourse from '../pages/admin/courseManagement/OfferCourse';
 import OfferedCourses from '../pages/admin/courseManagement/OfferedCourses';
 import AdminList from '../pages/admin/userManagement/AdminData';
@@ -34,27 +31,27 @@ export const adminPaths = [
         element: <CreateAcademicSemester />,
       },
       {
-        name: 'Academic Semester',
+        name: 'All Academic Semester',
         path: 'academic-semester',
         element: <AcademicSemester />,
       },
-      {
-        name: 'Create A. Faculty',
-        path: 'create-academic-faculty',
-        element: <CreateAcademicFaculty />,
-      },
-      {
-        name: 'Academic Faculty',
-        path: 'academic-faculty',
-        element: <AcademicFaculty />,
-      },
+      // {
+      //   name: 'Create A. Faculty',
+      //   path: 'create-academic-faculty',
+      //   element: <CreateAcademicFaculty />,
+      // },
+      // {
+      //   name: 'Academic Faculty',
+      //   path: 'academic-faculty',
+      //   element: <AcademicFaculty />,
+      // },
       {
         name: 'Create A. Department',
         path: 'create-academic-department',
         element: <CreateAcademicDepartment />,
       },
       {
-        name: 'Academic Department',
+        name: 'All A. Department',
         path: 'academic-department',
         element: <AcademicDepartment />,
       },
@@ -117,11 +114,6 @@ export const adminPaths = [
         name: 'Create Course',
         path: 'create-course',
         element: <CreateCourse />,
-      },
-      {
-        name: 'Courses',
-        path: 'courses',
-        element: <Courses />,
       },
       {
         name: 'Offer Course',
