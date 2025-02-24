09c23df1126811ab5972cb59dcbfb4056ab16350
Naimur Rahman <naimur2231@gmail.com>
2025-02-24 23:17:22 +0600

    project updated

--- src/pages/faculty/MyCourses.tsx
+++ src/pages/faculty/MyCourses.tsx
@@ -1,44 +1,8 @@
-import { Button, Col, Flex } from 'antd';
-import CForm from '../../components/form/CForm';
-import CSelect from '../../components/form/CSelect';
-import { useGetAllFacultyCoursesQuery } from '../../redux/features/faculty/facultyCourses.api';
-import { useNavigate } from 'react-router-dom';
-import { FieldValues, SubmitHandler } from 'react-hook-form';
-
 const MyCourses = () => {
-  const { data: facultyCoursesData } = useGetAllFacultyCoursesQuery(undefined);
-  const navigate = useNavigate();
-
-  console.log(facultyCoursesData);
-
-  const semesterOptions = facultyCoursesData?.data?.map((item) => ({
-    label: `${item.academicSemester.name} ${item.academicSemester.year}`,
-    value: item.semesterRegistration._id,
-  }));
-
-  const courseOptions = facultyCoursesData?.data?.map((item) => ({
-    label: item.course.title,
-    value: item.course._id,
-  }));
-
-  const onSubmit: SubmitHandler<FieldValues> = (data) => {
-    navigate(`/faculty/courses/${data.semesterRegistration}/${data.course}`);
-  };
-
   return (
-    <Flex justify="center" align="center">
-      <Col span={6}>
-        <CForm onSubmit={onSubmit}>
-          <CSelect
-            options={semesterOptions}
-            name="semesterRegistration"
-            label="Semester"
-          />
-          <CSelect options={courseOptions} name="course" label="Course" />
-          <Button htmlType="submit">Submit</Button>
-        </CForm>
-      </Col>
-    </Flex>
+    <div>
+      <h1> This is my courses component </h1>
+    </div>
   );
 };
 
--- src/pages/student/MySchedule.tsx
+++ src/pages/student/MySchedule.tsx
@@ -1,24 +1,9 @@
-import { useGetAllEnrolledCoursesQuery } from '../../redux/features/student/studentCourseManagement.api';
+
 
 const MySchedule = () => {
-  const { data } = useGetAllEnrolledCoursesQuery(undefined);
-  console.log(data);
-
   return (
     <div>
-      {data?.data?.map((item) => {
-        return (
-          <div>
-            <div>{item.course.title}</div>
-            <div>{item.offeredCourse.section}</div>
-            <div>
-              {item.offeredCourse.days.map((item) => (
-                <span> {item}</span>
-              ))}
-            </div>
-          </div>
-        );
-      })}
+      <h1> This is my schedule component </h1>
     </div>
   );
 };
